e9e860de7844c270bd9b9f539e032af2034319bd
Azulinho <mail@azulinho.com>
2018-12-21 11:56:32 +0000

    moves the version number to the package

--- setup.py
+++ setup.py
@@ -3,21 +3,44 @@
 
 """The setup script."""
 
+import codecs
+import os
+import re
 from setuptools import setup
 
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """ reads parts """
+    with codecs.open(os.path.join(HERE, *parts), 'r') as blob:
+        return blob.read()
+
+
+def find_version(*file_paths):
+    """ finds __version___ in __init__.py files """
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 with open('README.rst') as readme_file:
-    readme = readme_file.read()
+    README = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+    HISTORY = history_file.read()
 
 with open('requirements.txt') as requirements_file:
-    requirements = requirements_file.read().splitlines()
+    REQUIREMENTS = requirements_file.read().splitlines()
 
-setup_requirements = ['pytest-runner', ]
+SETUP_REQUIREMENTS = ['pytest-runner', ]
 
 with open('requirements_dev.txt') as requirements_dev_file:
-    test_requirements = requirements_dev_file.read().splitlines()
+    TEST_REQUIREMENTS = requirements_dev_file.read().splitlines()
 
 setup(
     name='archops_os_updates',
@@ -31,16 +54,16 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
     description="Install All OS updates",
-    install_requires=requirements,
+    install_requires=REQUIREMENTS,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=README + '\n\n' + HISTORY,
     include_package_data=True,
     keywords='archops_os_updates',
     packages=['archops_os_updates'],
-    setup_requires=setup_requirements,
+    setup_requires=SETUP_REQUIREMENTS,
     test_suite='tests',
-    tests_require=test_requirements,
+    tests_require=TEST_REQUIREMENTS,
     url='https://github.com/archops/os_updates',
-    version='0.0.1',
+    version=find_version('archops_os_updates', '__init__.py'),
     zip_safe=False,
 )
