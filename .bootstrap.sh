a18f2aec30d3349fba4173c52a2d766dbe50f5bc
Azulinho <mail@azulinho.com>
2018-12-22 19:19:03 +0000

    tidy up .bootstrap.sh

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,24 +1,48 @@
 #!/usr/bin/env bash
+# .boostrap.sh
+# install required python dependencies
+
+
+echo "running bootstrap ...."
 
 if [ ! -e .venv ]; then
+    echo "initializing virtualenv..."
     virtualenv --python=`which python` .venv
     source .venv/bin/activate
+    echo "installing python requirements..."
     pip install -r requirements.txt
+    echo "installing development python requirements..."
     pip install -r requirements_dev.txt
+    echo "installing local source..."
     pip install -e .
   fi
 
-which pyenv || (test -e .pyenv || git clone https://github.com/pyenv/pyenv.git .pyenv )
-test -e .pyenv && export PYENV_ROOT="$PWD/.pyenv"
-test -e .pyenv && export PATH="$PYENV_ROOT/bin:$PATH"
 
+if ! `which pyenv >/dev/null 2>&1`; then
+    if ! -e .pyenv; then
+        echo "installing pyenv locally...."
+        git clone https://github.com/pyenv/pyenv.git .pyenv
+    else
+        echo ".pyenv found, using local pyenv"
+        export PYENV_ROOT="$PWD/.pyenv"
+        export PATH="$PYENV_ROOT/bin:$PATH"
+    fi
+fi
+
+echo "activating  pyenv...."
 eval "$(pyenv init -)"
 
+echo "activating  venv...."
 source .venv/bin/activate
 
-# install required python versions
-(pyenv versions | grep 3.7.1) || pyenv install 3.7.1
+local_python_version=`cat .python-version`
 
-pyenv local 3.7.1
+if ! `pyenv versions | grep ${local_python_version} >/dev/null 2>&1`; then
+    echo "installing python version ${local_python_version} using pyenv"
+    pyenv install ${local_python_version}
+fi
 
+echo "running test for the first time...."
 test -e .tox || tox
+
+echo "finished bootstrapping..."
