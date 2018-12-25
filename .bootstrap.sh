33d491dcba74d15bf24faa36227ad4c41c3df6ee
Azulinho <mail@azulinho.com>
2018-12-25 11:53:33 +0000

    we need to source this up

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -26,6 +26,7 @@ if ! `which pyenv >/dev/null 2>&1`; then
     curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
     echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bashrc
     echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+    source ~/.bashrc
 fi
 
 echo "${bold}activating  pyenv....${normal}"
